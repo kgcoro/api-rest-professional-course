c3a190f08a84f5b6ff15b9a70b702ce0788119ca
Kleber Coro <kgcoro@gmail.com>
2023-03-16 16:50:34 -0500

    categories movie

--- src/main.js
+++ src/main.js
@@ -15,11 +15,37 @@ async function getTrendingMoviesPreview() {
         movieImg.classList.add('movie-img');
 
         movieImg.setAttribute('alt', movie.title);
-        movieImg.setAttribute('src', `https://image.tmdb.org/t/p/w300/${movie.poster_path}`);
+        movieImg.setAttribute('src', `https://image.tmdb.org/t/p/w300${movie.poster_path}`);
 
         movieContainer.appendChild(movieImg)
         trendingPreviewMoviesContainer.appendChild(movieContainer)
     });
 }
 
+async function getCategoriesMoviesPreview() {
+    const res = await fetch(`https://api.themoviedb.org/3/genre/movie/list?api_key=${API_KEY}` );
+    const data = await res.json();
+
+    const categories = data.genres;
+    categories.forEach(category => {
+        const categoriesPreviewContainer = document.querySelector
+        ('#categoriesPreview .categoriesPreview-list')
+
+        const categoryContainer = document.createElement('div');
+        categoryContainer.classList.add('category-container')
+
+        const categoryTitle = document.createElement('h3');
+        categoryTitle.classList.add('category-title');
+
+        categoryTitle.setAttribute('id', `id${category.id}`);
+        const categoryTitleText = document.createTextNode(category.name);
+
+        categoryTitle.appendChild(categoryTitleText);
+
+        categoryContainer.appendChild(categoryTitle);
+        categoriesPreviewContainer.appendChild(categoryContainer);
+    });
+}
+
 getTrendingMoviesPreview();
+getCategoriesMoviesPreview();
