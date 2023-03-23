7694f92d746ef48aff2f859dbd3c389d9f1691c9
Kleber Coro <kgcoro@gmail.com>
2023-03-22 23:05:30 -0500

    fix duplicate data error

--- src/main.js
+++ src/main.js
@@ -14,9 +14,9 @@ async function getTrendingMoviesPreview() {
 
     const movies = data.results;
 
+    trendingMoviesPreviewList.innerHTML = "";
+
     movies.forEach(movie => {
-        const trendingMoviesPreviewList = document.querySelector
-        ('#trendingPreview .trendingPreview-movieList')
 
         const movieContainer = document.createElement('div');
         movieContainer.classList.add('movie-container')
@@ -34,12 +34,12 @@ async function getTrendingMoviesPreview() {
 
 async function getCategoriesMoviesPreview() {
     const { data } = await api(`genre/movie/list` );
-
+    
     const categories = data.genres;
 
+    categoriesPreviewList.innerHTML = "";
+
     categories.forEach(category => {
-        const categoriesPreviewList = document.querySelector
-        ('#categoriesPreview .categoriesPreview-list')
 
         const categoryContainer = document.createElement('div');
         categoryContainer.classList.add('category-container')
